ca3575b2072d51ef464532128e56bf9679c15e6b
Abhishek <abhisheksunny463@gmail.com>
2024-01-11 22:17:06 +0530

    Explore about server how we make rosuting in backend

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,31 @@
 const http = require('http')
 const fs = require('fs')
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: New Request Received \n`;
+    const log = `${Date.now()}: ${req.url} New Request Received \n`;
+    const myUrl = url.parse(req.url, true)
+
+    console.log("Url", myUrl)
+
     fs.appendFile("./log.txt", log, (err, data) => {
-        res.end("Hello From Server");
+
+        switch (myUrl.pathname) {
+            case "/":
+                res.end("Home Page");
+                break;
+            case '/about':
+                const username = myUrl.query.myname
+                res.end(`Hii, ${username}`);
+                break;
+            case "/search":
+                const search = myUrl.query.q;
+                res.end(`Here are your result for ${search}` );
+                break;
+            default:
+                res.end("404 Page Not Found")
+
+        }
     })
 })
 
