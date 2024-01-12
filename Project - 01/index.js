211e0c014625e194eaea941cd4406a33bf181a8b
Abhishek <abhisheksunny463@gmail.com>
2024-01-13 01:00:15 +0530

    Connect Mongodb with project and apply CRUD funtionality on data that comes from database

--- Project - 01/index.js
+++ Project - 01/index.js
@@ -1,9 +1,43 @@
 const express = require("express")
-const users = require('./MOCK_DATA.json')
 const fs = require("fs")
+const mongoose = require("mongoose")
 
 const app = express()
 const port = 8000
+
+// mongo db connection
+mongoose.connect("mongodb://localhost:27017/first-database")
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.log("Mongo Error", err))
+
+
+
+// schema
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    jobTitle: {
+        type: String,
+        required: true
+    }
+}, {timestamps:true})
+
+const User = mongoose.model("user", userSchema)
 
 
 // Middleware - Plugin
@@ -12,10 +46,11 @@ app.use(express.urlencoded({ extended: false }))
 // Routing
 
 // This route send html data and this is called server side render page
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const allDbUsers = await User.find({})
     const html = `
     <ul>
-        ${users.map((user) => `<li>${user.first_name}.</li>`).join("")}
+        ${allDbUsers.map((user) => `<li>${user.firstName} - ${user.email}</li>`).join("")}
     </ul>
     `
     return res.send(html)
@@ -24,24 +59,40 @@ app.get('/users', (req, res) => {
 
 // This route will return json data 
 // This route return all the users 
-app.get('/api/users', (req, res) => {
-    return res.json(users)
+app.get('/api/users', async (req, res) => {
+    const allDbUsers = await User.find({})
+    return res.json(allDbUsers)
 })
 
 // This route will return the data of specific user on the basic of id
-app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id == id)
+app.get("/api/users/:id", async (req, res) => {
+
+    const user = await User.findById(req.params.id)
+    if (!user) return res.status(404).json({ error: "User not found" })
     return res.json(user)
 })
 
 // This route will create a new user
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
     const body = req.body;
-    users.push({ id: users.length + 1, ...body })
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-        return res.json({ status: "fulfilled", id: users.length })
+    if (!body || !body.firstName || !body.lastName || !body.email || !body.gender || !body.jobTitle) {
+        return res.status(400).json({ message: "All Fields are Required..." })
+    }
+    // users.push({ id: users.length + 1, ...body })
+    // fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+    //     return res.status(201).json({ status: "fulfilled", id: users.length })
+    // })
+
+    const result = await User.create({
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email,
+        gender: body.gender,
+        jobTitle: body.jobTitle
     })
+
+    return res.status(201).json({message: "User Created Successfully"})
+
 })
 
 //This route will update the user data on the basic of id
@@ -50,10 +101,9 @@ app.patch("/api/users/:id", (req, res) => {
 })
 
 // This route will delete the user on the basic of id
-app.delete("/api/users/:id", (req, res) => {
-    const id = parseInt(req.params.id)
-    const user = users.filter((user)=> user.id !== id)
-    res.json({ message: 'Item deleted successfully', user });
+app.delete("/api/users/:id", async (req, res) => {
+    await User.findByIdAndDelete(req.params.id)
+    res.json({ message: 'Item deleted successfully'});
 })
 
 app.listen(port, () => console.log("Server Started at Port " + port))
